bdf1bc582689db8a333c9f26a1803ad988bed5ea
Thomas Nizan <tnizan@witekio.com>
2022-10-14 09:58:49 -0700

    sources: webpages: rework webpages
    - add controls for the RGB LEDs, the temperature and pressure sensors
    - add Zu board materials (pictures, documentation...)

--- sources/webpages/js/main.js
+++ sources/webpages/js/main.js
@@ -1,46 +1,93 @@
 var webserver = {
     init: function() {
-        /* obtain a pointer to the led_form and its result */
-        webserver.led_form = document.getElementById('led_form');
-        webserver.led_results_div = document.getElementById('led_results');
+        /* obtain a pointer to the rgbled0_form and its result */
+        webserver.rgbled0_form = document.getElementById('rgbled0_form');
+        webserver.rgbled0_status_div = document.getElementById('rgbled0_status');
 
         /* listen for the submit button press */
-        YAHOO.util.Event.addListener(webserver.led_form, 'submit', webserver.led_submit);
+        YAHOO.util.Event.addListener(webserver.rgbled0_form, 'submit', webserver.led_submit);
 
-        /* obtain a pointer to the led_form and its result */
+        /* obtain a pointer to the rgbled1_form and its result */
+        webserver.rgbled1_form = document.getElementById('rgbled1_form');
+        webserver.rgbled1_status_div = document.getElementById('rgbled1_status');
+
+        /* listen for the submit button press */
+        YAHOO.util.Event.addListener(webserver.rgbled1_form, 'submit', webserver.led_submit);
+
+        /* obtain a pointer to the switch_form and its result */
         webserver.switch_form = document.getElementById('switch_form');
         webserver.switch_status_div = document.getElementById('switch_status');
 
         /* listen for the submit button press */
         YAHOO.util.Event.addListener(webserver.switch_form, 'submit', webserver.switch_submit);
 
+        /* obtain a pointer to the temp_form and its result */
+        webserver.temp_form = document.getElementById('temp_form');
+        webserver.temp_results_div = document.getElementById('temp_results');
+
+        /* listen for the submit button press */
+        YAHOO.util.Event.addListener(webserver.temp_form, 'submit', webserver.temp_submit);
+
+        /* obtain a pointer to the pressure_form and its result */
+        webserver.pressure_form = document.getElementById('pressure_form');
+        webserver.pressure_results_div = document.getElementById('pressure_results');
+
+        /* listen for the submit button press */
+        YAHOO.util.Event.addListener(webserver.pressure_form, 'submit', webserver.pressure_submit);
+
         /* make a XMLHTTP request */
         YAHOO.util.Connect.asyncRequest('POST', '/cmd/switchxhr', webserver.switch_callback);
-        YAHOO.util.Connect.asyncRequest('POST', '/cmd/ledxhr', webserver.led_callback);
     },
-   
+
     led_submit: function(e) {
         YAHOO.util.Event.preventDefault(e);
-        YAHOO.util.Connect.setForm(webserver.led_form);
-
-        /* disable the form until result is obtained */
-        for(var i=0; i<webserver.led_form.elements.length; i++) {
-            webserver.led_form.elements[i].disabled = true;
-        }
+        form = e.target;
+        YAHOO.util.Connect.setForm(form);
 
         /* make a XMLHTTP request */
         YAHOO.util.Connect.asyncRequest('POST', '/cmd/ledxhr', webserver.led_callback);
-
-        /* fade in the results */
-        // webserver.toggle_led_display();
-        
-        var result_fade_out = new YAHOO.util.Anim(webserver.led_results_div, 
-                                    {opacity: { to: 0 }}, 
-                                    0.25, YAHOO.util.Easing.easeOut);
-        result_fade_out.animate();
     },
 
-   switch_submit: function(e) {
+    led_callback: {
+        success: function(o) {
+            /* This turns the JSON string into a JavaScript object. */
+            a = o.responseText.split('&');
+            rgbled=a[0].split('=')[1]
+            color=a[1].split('=')[1]
+
+            if(rgbled != 0 && rgbled != 1) {
+                alert('LEDXHR request failed: unknown LED ');
+            }
+
+            /* Modify led box */
+            led_status_div = eval("webserver.rgbled"+rgbled+"_status_div")
+
+            switch(color) {
+                case "0":
+                led_status_div.innerHTML='<div class="led-off"></div>';
+                break;
+                case "1":
+                led_status_div.innerHTML='<div class="led-red"></div>';
+                break;
+                case "2":
+                led_status_div.innerHTML='<div class="led-green"></div>';
+                break;
+                case "3":
+                led_status_div.innerHTML='<div class="led-blue"></div>';
+                break;
+                default:
+                led_status_div.innerHTML='<p><span style="background:#fb6767"> ERROR </span></p>';
+            }
+        },
+
+        failure: function(o) {
+            alert('LEDXHR request failed: ' + o.statusText);
+        },
+
+        timeout: 3000
+    },
+
+    switch_submit: function(e) {
         YAHOO.util.Event.preventDefault(e);
         YAHOO.util.Connect.setForm(webserver.switch_form);
 
@@ -52,119 +99,154 @@ var webserver = {
         /* make a XMLHTTP request */
         YAHOO.util.Connect.asyncRequest('POST', '/cmd/switchxhr', webserver.switch_callback);
 
-        /* fade in the results */
-        // webserver.toggle_switch_display();
-
         var result_fade_out = new YAHOO.util.Anim(webserver.switch_status_div, 
-                                    {opacity: { to: 0 }}, 
-                                    0.25, YAHOO.util.Easing.easeOut);
+            {opacity: { to: 0 }},
+            0.25, YAHOO.util.Easing.easeOut);
         result_fade_out.animate();
-   },
+    },
 
-   toggle_led_display: function() {
-        var result_fade_out = new YAHOO.util.Anim(webserver.led_results_div, {
-                                              opacity: { to: 0 }
-                                           }, 0.25, YAHOO.util.Easing.easeOut);
-        var result_fade_in = new YAHOO.util.Anim(webserver.led_results_div, {
-                                              opacity: { to: 1 }
-                                           }, 0, YAHOO.util.Easing.easeOut);
-
-        /* fade in the new result */
-        result_fade_out.onComplete.subscribe(function() {
-                                         /* Re-enable the form. */
-                                         for(var i=0; i<webserver.led_form.elements.length; i++) {
-                                                webserver.led_form.elements[i].disabled = false;
-                                         };
-                                         result_fade_in.animate();
-                                         });
-
-        /* fade out the existing result */
-        result_fade_out.animate();
-   },
-   
-   toggle_switch_display: function() {
-        var result_fade_out = new YAHOO.util.Anim(webserver.switch_status_div, {
-                                              opacity: { to: 0 }
-                                           }, 0.25, YAHOO.util.Easing.easeOut);
-        var result_fade_in = new YAHOO.util.Anim(webserver.switch_status_div, {
-                                              opacity: { to: 1 }
-                                           }, 0, YAHOO.util.Easing.easeOut);
-
-        /* fade in the new result */
-        result_fade_out.onComplete.subscribe(function() {
-                                         /* Re-enable the form. */
-                                         for(var i=0; i<webserver.switch_form.elements.length; i++) {
-                                                webserver.switch_form.elements[i].disabled = false;
-                                         };
-                                         result_fade_in.animate();
-                                         });
-
-        /* fade out the existing result */
-        result_fade_out.animate();
-   },
-   
-   led_callback: {
-      success: function(o) {
+    switch_callback: {
+        success: function(o) {
             /* This turns the JSON string into a JavaScript object. */
-            var response_obj = eval('(' + o.responseText + ')');
-             
-            // Set up the animation on the results div.
-            var result_fade_in = new YAHOO.util.Anim(webserver.led_results_div, {
-                                                        opacity: { to: 1 }
-                                                        }, 0.25, YAHOO.util.Easing.easeIn);
+            var response = eval('(' + o.responseText + ')');
 
-            if (response_obj == 0)
-                webserver.led_results_div.innerHTML = '<p><span style="background:#ffffaa">LEDs are now OFF.</span></p>';
-            else
-                webserver.led_results_div.innerHTML = '<p><span style="background:#33ff00">LEDs are now ON.</span></p>';
+            // Set up the animation on the results div.
+            var result_fade_in = new YAHOO.util.Anim(webserver.switch_status_div, {
+                opacity: { to: 1 }
+            }, 0.25, YAHOO.util.Easing.easeIn);
+
+            webserver.switch_status_div.innerHTML = '<p><span style="background:#33ff00"> ' + o.responseText + ' </span></p>';
 
             /* enable after fade int */
             result_fade_in.onComplete.subscribe(function() {
-                          /* Re-enable the form. */
-                          for(var i=0; i<webserver.led_form.elements.length; i++) {
-                                 webserver.led_form.elements[i].disabled = false;
-                          };
-                          });
+                /* Re-enable the form. */
+                for(var i=0; i<webserver.switch_form.elements.length; i++) {
+                    webserver.switch_form.elements[i].disabled = false;
+                };
+            });
 
             result_fade_in.animate();
-      }
-   },
+        },
 
-   switch_callback: {
-      success: function(o) {
-	 /* This turns the JSON string into a JavaScript object. */
-	 var response = eval('(' + o.responseText + ')');
-	 
-	 // Set up the animation on the results div.
-	 var result_fade_in = new YAHOO.util.Anim(webserver.switch_status_div, {
-							opacity: { to: 1 }
-						     }, 0.25, YAHOO.util.Easing.easeIn);
+        failure: function(o) {
+            // Set up the animation on the results div.
+            var result_fade_in = new YAHOO.util.Anim(webserver.switch_status_div, {
+                opacity: { to: 1 }
+            }, 0.25, YAHOO.util.Easing.easeIn);
 
-         webserver.switch_status_div.innerHTML = '<p><span style="background:#33ff00"> ' + o.responseText + ' </span></p>';
+            webserver.switch_status_div.innerHTML = '<p><span style="background:#fb6767"> ERROR </span></p>';
 
-         /* enable after fade int */
-         result_fade_in.onComplete.subscribe(function() {
-                                         /* Re-enable the form. */
-                                         for(var i=0; i<webserver.switch_form.elements.length; i++) {
-                                                webserver.switch_form.elements[i].disabled = false;
-                                         };
-                                         });
+            /* enable after fade int */
+            result_fade_in.onComplete.subscribe(function() {
+                /* Re-enable the form. */
+                for(var i=0; i<webserver.switch_form.elements.length; i++) {
+                    webserver.switch_form.elements[i].disabled = false;
+                };
+            });
 
-	 result_fade_in.animate();
-      },
+            result_fade_in.animate();
+        },
 
-      failure: function(o) {
-                   alert('XHR request failed: ' + o.statusText);
-      },
+        timeout: 3000
+    },
 
-      timeout: 10000
-   }
+    temp_submit: function(e) {
+        YAHOO.util.Event.preventDefault(e);
+        YAHOO.util.Connect.setForm(webserver.temp_form);
+
+        /* make a XMLHTTP request */
+        YAHOO.util.Connect.asyncRequest('POST', '/cmd/tempxhr', webserver.temp_callback);
+
+        var result_fade_out = new YAHOO.util.Anim(webserver.temp_results_div,
+            {opacity: { to: 0 }},
+            0.25, YAHOO.util.Easing.easeOut);
+        result_fade_out.animate();
+    },
+
+    temp_callback: {
+        success: function(o) {
+            /* This turns the JSON string into a JavaScript object. */
+            var response_obj = eval('(' + o.responseText + ')');
+
+            // Set up the animation on the results div.
+            var result_fade_in = new YAHOO.util.Anim(webserver.temp_results_div, {
+                opacity: { to: 1 }
+            }, 0.25, YAHOO.util.Easing.easeIn);
+
+            if (!response_obj)
+                webserver.temp_results_div.innerHTML = '<p><span style="background:#fb6767">Error getting temperature.</span></p>';
+            else
+                webserver.temp_results_div.innerHTML = '<p><span style="background:#33ff00">Temperature is ' + o.responseText + ' &#8451;.</span></p>';
+
+            result_fade_in.animate();
+        },
+
+        failure: function(o) {
+
+            // Set up the animation on the results div.
+            var result_fade_in = new YAHOO.util.Anim(webserver.temp_results_div, {
+                opacity: { to: 1 }
+            }, 0.25, YAHOO.util.Easing.easeIn);                
+
+            webserver.temp_results_div.innerHTML = '<p><span style="background:#fb6767">Error getting temperature.</span></p>';
+
+            result_fade_in.animate();
+        },
+
+        timeout: 3000
+    },
+
+    pressure_submit: function(e) {
+        YAHOO.util.Event.preventDefault(e);
+        YAHOO.util.Connect.setForm(webserver.pressure_form);
+
+        /* make a XMLHTTP request */
+        YAHOO.util.Connect.asyncRequest('POST', '/cmd/pressurexhr', webserver.pressure_callback);
+
+        var result_fade_out = new YAHOO.util.Anim(webserver.pressure_results_div,
+            {opacity: { to: 0 }},
+            0.25, YAHOO.util.Easing.easeOut);
+        result_fade_out.animate();
+    },
+
+    pressure_callback: {
+        success: function(o) {
+            /* This turns the JSON string into a JavaScript object. */
+            var response_obj = eval('(' + o.responseText + ')');
+
+            // Set up the animation on the results div.
+            var result_fade_in = new YAHOO.util.Anim(webserver.pressure_results_div, {
+                opacity: { to: 1 }
+            }, 0.25, YAHOO.util.Easing.easeIn);
+
+            if (!response_obj)
+                webserver.pressure_results_div.innerHTML = '<p><span style="background:#fb6767">Error getting pressure.</span></p>';
+            else
+                webserver.pressure_results_div.innerHTML = '<p><span style="background:#33ff00">Pressure is ' + o.responseText + ' hPa.</span></p>';
+
+            result_fade_in.animate();
+        },
+
+        failure: function(o) {
+
+            // Set up the animation on the results div.
+            var result_fade_in = new YAHOO.util.Anim(webserver.pressure_results_div, {
+                opacity: { to: 1 }
+            }, 0.25, YAHOO.util.Easing.easeIn);
+
+            webserver.pressure_results_div.innerHTML = '<p><span style="background:#fb6767">Error getting pressure.</span></p>';
+
+            result_fade_in.animate();
+        },
+
+        timeout: 3000
+    }
 };
 
 YAHOO.util.Event.addListener(window, 'load', webserver.init);
 
 function popup(url) {
-	newwindow = window.open(url, 'image', 'height=400,width=550,resizeable=1,scrollbards,menubar=0,toolbar=0');
-	if (window.focus) {newwindow.focus()}
-	return false;
+    newwindow = window.open(url, 'image', 'height=500,width=800,resizeable=1,scrollbards,menubar=0,toolbar=0');
+    if (window.focus) {newwindow.focus()}
+    return false;
 }
