8a1da7c8d035705b137bebb43b06a19629ef2b3b
Thomas Nizan <tnizan@witekio.com>
2022-10-14 09:51:47 -0700

    sources: apps: oob: gpios: add support for rgl leds and push buttons
    - add a function to control the RGB LEDs.
    
    - add an interrupt handler to get the status of the MIO SW and the MIO
    PB: the switches control the MIO LEDs. The PB will turn all of them RED.
    
    - add a polling task to get the status of the PL PB. It turns the two
    RGB LEDs to RED when pressed.

--- sources/apps/oob/src/platform_gpio.h
+++ sources/apps/oob/src/platform_gpio.h
@@ -1,8 +1,15 @@
 #ifndef __PLATFORM_GPIO_H_
 #define __PLATFORM_GPIO_H_
 
-void platform_init_gpios();
-int toggle_leds();
+typedef enum color {
+	COLOR_NONE,
+	COLOR_RED,
+	COLOR_GREEN,
+	COLOR_BLUE
+} color_t;
+
+int platform_init_gpios();
+int control_rgb_leds(int led_index, color_t color);
 unsigned int get_switch_state();
 
 #endif
